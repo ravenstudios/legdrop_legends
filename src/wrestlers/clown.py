f2d02ab0ae89b41fc9cda10854c7862d96a54275
ravenstudios <ravenstudios@Robs-MacBook-Pro.local>
2025-06-19 18:46:34 -0500

    fixed clown stats

--- src/wrestlers/clown.py
+++ src/wrestlers/clown.py
@@ -11,31 +11,30 @@ class Clown(npc.NPC):
     def __init__(self):
         super().__init__(400, 200, "clown_16x16-Sheet.png")
         self.battle_object = enemy_battle_object.EnemyBattleObject(500, 50, "clown_32x32.png", 1)
-        self.battle_object.hp = 50
-        self.battle_object.mp = 30
-        self.battle_object.max_hp = 75
-        self.battle_object.max_mp = 50
+        self.battle_object.hp = 30
+        self.battle_object.mp = 20
+        self.battle_object.max_hp = 30
+        self.battle_object.max_mp = 20
+        self.battle_object.powder_rate = 10
         self.battle_object.name = "Funny Guy"
         self.battle_object.options = {
             "Attacks": [
-                {"name": "Chop", "dmg": 3, "cost": 2, "type":"attack", "message":"Player used Chop"},
-                {"name": "Kick", "dmg": 5, "cost": 2, "type":"attack", "message":"Player used Kick"},
-                {"name": "Back"}
-            ],
+                {"name": "Chop", "dmg": 3, "cost": 2, "type":"attack", "message":"Funny Guy used Chop, it dealt 3 damage"},
+                {"name": "Kick", "dmg": 5, "cost": 2, "type":"attack", "message":"Funny Guy used Kick, it dealt 5 damage"},
+                            ],
             "Items": [
-                {"name": "Bandaid", "hp": 5, "type":"restore_hp", "message":"Player used Bandaid"},
-                {"name": "Beer", "mp": 10, "type":"restore_mp", "message":"Player used Beer"},
-                {"name": "Powder", "mp": 5, "type":"restore_mp", "message":"Player powered"},
-                {"name": "Back"}
-            ],
+                {"name": "Bandaid", "hp": 5, "type":"restore_hp", "message":"Funny Guy used Bandaid"},
+                {"name": "Beer", "mp": 10, "type":"restore_mp", "message":"Funny Guy used Beer"},
+                {"name": "Powder", "mp": 5, "type":"restore_mp", "message":"Funny Guy powered"},
+                            ],
             "Tag Partner": [
                 {"name": "Tag Partner", "type":"tag", "message":"Player tagged"},
-                {"name": "Back"},
+
 
             ],
             "Powder": [
                 {"name": "Powder", "mp": 0, "type":"restore_mp", "message":""},
-                {"name": "Back"},
+
             ],
 
             "Run": [
